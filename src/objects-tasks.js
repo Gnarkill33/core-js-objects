9978a509fae1400f52a87bebfb6a68ab6ed4cb4f
Gnarkill33 <gnarkill33@gmail.com>
2025-02-25 21:05:04 +0300

    feat: complete makeImmutable, makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -119,8 +119,8 @@ function isEmptyObject(obj) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
@@ -133,8 +133,20 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  if (Object.keys(lettersObject).length === 0) return '';
+  const valueArray = Object.values(lettersObject);
+  const entryArray = Object.entries(lettersObject);
+  const maxIndex = Math.max(...valueArray.flat());
+  const result = new Array(maxIndex + 1).fill('');
+
+  entryArray.forEach(([key, value]) => {
+    value.forEach((index) => {
+      result[index] = key;
+    });
+  });
+
+  return result.join('');
 }
 
 /**
